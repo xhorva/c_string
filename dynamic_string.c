70fd6987926c3d21a2b6ed91536a6d2bdd6887e1
ares <martin.horvat@protonmail.com>
2022-12-08 18:30:28 +0100

    Initial commit.

---
+++ dynamic_string.c
@@ -0,0 +1,147 @@
+/**
+ * @file dynamic_string.h
+ * @author Martin Horvat
+ * @brief Implementation of dynamic string.
+ * @date 2022-10-05
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include "dynamic_string.h"
+
+#define DS_SIZE 128
+
+dynamic_string_t * ds_init() {
+	dynamic_string_t * ds = (dynamic_string_t*) calloc(1, sizeof(dynamic_string_t)); // Allocate ds struct.
+	if(ds == NULL) {
+		return NULL;
+	}
+	char * str = (char*) calloc(1, DS_SIZE*sizeof(char)); // Allocate string.
+	if(str == NULL) {
+		free(ds);
+		return NULL;
+	}
+	ds->str = str;
+	ds->size = DS_SIZE;
+	ds->wi = 0;
+	return ds;
+}
+
+dynamic_string_t * ds_strinit(const char * str) {
+	if(str == NULL) {
+		return ds_init(); // Return empty dynamic_string if string is NULL.
+	}
+	dynamic_string_t * ds = (dynamic_string_t*) calloc(1, sizeof(dynamic_string_t)); // Allocate ds struct
+	if(ds == NULL) {
+		return NULL;
+	}
+	size_t size = DS_SIZE;
+	while(size <= strlen(str)) {
+		size *= 2;
+	}
+	ds->str = (char*) calloc(1, size*sizeof(char)); // Allocate string buffer with write reserve.
+
+	if(ds->str == NULL) {
+		free(ds);
+		return NULL;
+	}
+
+	strcpy(ds->str, str); // Attach to dynamic string...
+	ds->size = size;
+	ds->wi = strlen(str); // First index after end of string.
+
+	return ds;
+}
+
+int ds_resize(dynamic_string_t * ds) {
+	char * new_str = (char*) realloc(ds->str, ds->size*2); // Double size of string.
+	if(new_str == NULL) {
+		return 1;
+	}
+	ds->str = new_str;
+	ds->size = ds->size*2;
+	return 0;
+}
+
+void ds_dstr(dynamic_string_t * ds) {
+	if(ds == NULL) {
+		return;
+	}
+	ds->size = 0;
+	ds->wi = 0;
+	free(ds->str);
+	free(ds); // Free self.
+}
+
+int ds_write(dynamic_string_t * ds, int c) {
+	ds->str[ds->wi] = c;
+	ds->wi++;
+	if(ds->wi == ds->size) { // Resize if string limit reached.
+		if(ds_resize(ds)) { // Handle realloc error.
+			ds_dstr(ds); // De-init self if error occured.
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int ds_concat(dynamic_string_t * a, dynamic_string_t * b) {
+	if(a == NULL || b == NULL) {
+		return 1;
+	}
+	if(ds_concat_str(a, b->str)) { // Do as concatenation of string only.
+		return 1;
+	}
+	return 0;
+}
+
+int ds_concat_str(dynamic_string_t * ds, const char * str) {
+	if(str == NULL || ds == NULL || ds->str == NULL) {
+		return 0; // Change nothing when NULL string.
+	}
+	size_t new_size = ds->size;
+	while(new_size <= strlen(ds->str) + strlen(str)) { // Increase size to fit both strings.
+		new_size *= 2;
+	}
+	char * new_str = (char*) realloc(ds->str, new_size);
+	if(new_str == NULL) { // Allocation failure.
+		return 1;
+	}
+	strcat(new_str, str);
+	ds->str = new_str;
+	ds->size = new_size;
+	ds->wi = strlen(new_str);
+	return 0;
+}
+
+int ds_write_uint(dynamic_string_t * ds, unsigned n) {
+	if(ds == NULL) {
+		return 1;
+	}
+	size_t size = n;
+	if(n == 0) { // Resizing for 0...
+		size++;
+	}
+	size_t dummy_size =  (int)((ceil(log10(size))+2)*sizeof(char)); // Get dummy size by logarithm of n.
+	char dummy[dummy_size];
+	sprintf(dummy, "%u", n); // Write as unsigned into dummy.
+	if(ds_concat_str(ds, dummy)) {
+		return 1;
+	}
+	return 0;
+}
+
+char * ds_get_str(dynamic_string_t * ds, bool free_ds) {
+	char * str = malloc(strlen(ds->str) + 1);
+	if(str == NULL) { // Allocation failure.
+		return NULL;
+	}
+	strcpy(str, ds->str);
+	if(free_ds) {
+		ds_dstr(ds);
+	}
+	return str;
+}
---
+++ dynamic_string.h
@@ -0,0 +1,83 @@
+/**
+ * @file dynamic_string.h
+ * @author Martin Horvat
+ * @brief Header file for dynamic string.
+ * @date 2022-10-05
+ */
+
+#ifndef DYNAMIC_STRING_H
+#define DYNAMIC_STRING_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+	char * str; // String to write into.
+	size_t size; // Size of currently allocated string buffer.
+	unsigned wi; // Next index of writing into str.
+} dynamic_string_t;
+
+/**
+ * @brief Initialize an empty dynamic string.
+ * @returns Initialized dynamic string. NULL if error.
+ */
+dynamic_string_t * ds_init();
+
+/**
+ * @brief Initialize dynamic string with a given string.
+ * @param str String with which the ds is initialized.
+ * @returns Initialized dynamic string. NULL if error.
+*/
+dynamic_string_t * ds_strinit(const char * str);
+
+/**
+ * @brief Resize dynamic string to twice the current size.
+ * @param ds Dynamic string to resize.
+ * @returns 0 when successful, non-zero on error.
+ */
+int ds_resize(dynamic_string_t * ds);
+
+/**
+ * @brief Deallocates dynamic string.
+ * @param ds Dynamic string to deallocate.
+ */
+void ds_dstr(dynamic_string_t * ds);
+
+/**
+ * @brief Writes character at end of dynamic string. On error, string is deallocated.
+ * @param ds Dynamic string for writing.
+ * @param c Character to write.
+ * @returns 0 when successful, non-zero on error.
+ */
+int ds_write(dynamic_string_t * ds, int c);
+
+/**
+ * @brief Concatenates the dynamic string from a onto b. Operation: a.b
+ * @param a Dynamic string which will be concatenated.
+ * @param b Dyanmic string which is concatenated onto a.
+ */
+int ds_concat(dynamic_string_t * a, dynamic_string_t * b);
+
+/**
+ * @brief Concatenates a string literal onto a dynamic string.
+ * @param ds Dynamic string which will be concatenated.
+ * @param str String literal which is concatenated onto ds.
+*/
+int ds_concat_str(dynamic_string_t * ds, const char * str);
+
+/**
+ * @brief Writes an unsigned integer as a string value onto the end of a dynamic string.
+ * @param ds Dynamic string for writing.
+ * @param n Integer to write.
+*/
+int ds_write_uint(dynamic_string_t * ds, unsigned n);
+
+/**
+ * @brief Returns the string contained in ds.
+ * @param ds Dynamic string with string.
+ * @param free_ds Deinitialize ds if true.
+ * @returns String from ds, NULL on failure.
+*/
+char * ds_get_str(dynamic_string_t * ds, bool free_ds);
+
+#endif // DYNAMIC_STRING_H
